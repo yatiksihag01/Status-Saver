ee3e076a458f0e7d43e65cbbd17c2b4f5a304009
Yatik <yatiksihag01@gmail.com>
2022-09-27 22:33:34 +0530

    permission for android less than 10

--- app/src/main/java/com/yatik/statussaver/MainActivity.java
+++ app/src/main/java/com/yatik/statussaver/MainActivity.java
@@ -1,34 +1,103 @@
 package com.yatik.statussaver;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Toast;
 
-import java.util.ArrayList;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import com.yatik.statussaver.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ArrayList<ImageData> imageData;
-    private RecyclerView recyclerView;
+    ActivityResultLauncher<String> mPermissionResultLauncher;
+    private boolean isWritePermissionGranted = false;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
 
-        recyclerView = findViewById(R.id.recyclerView);
-        imageData = new ArrayList<>();
+        mPermissionResultLauncher =
+                registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                    if (isGranted) {
+                        Toast.makeText(this, "Permission Granted!", Toast.LENGTH_SHORT).show();
+                    } else {
+                        noPermissionAlert();
+                    }
 
-        imageData.add(new ImageData(R.drawable.family_mother));
-        imageData.add(new ImageData(R.drawable.family_older_sister));
-        imageData.add(new ImageData(R.drawable.family_son));
+                });
+        requestPermission();
 
-        CustomAdapter adapter = new CustomAdapter(imageData);
-        GridLayoutManager layoutManager = new GridLayoutManager(this, 2);
-        recyclerView.setLayoutManager(layoutManager);
-        recyclerView.setAdapter(adapter);
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
 
+            switch (item.getItemId()) {
+                case (R.id.images):
+                    replaceFragment(new ImageFragment());
+                    break;
+                case (R.id.videos):
+                    replaceFragment(new VideoFragment());
+                    break;
+                case (R.id.downloads):
+                    replaceFragment(new DownloadsFragment());
+                    break;
+            }
+            return true;
+        });
     }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.main_frame_layout, fragment, null)
+                .setReorderingAllowed(true)
+                .commit();
+    }
+
+    private void noPermissionAlert(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle("Permission Denied")
+//                .setView(R.layout.permission_denied)
+                .setCancelable(false)
+                .setIcon(R.drawable.permission_message)
+                .setPositiveButton(R.string.settings, (dialog, which) -> {
+                    Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                    Uri uri = Uri.fromParts("package", getPackageName(), null);
+                    intent.setData(uri);
+                    startActivity(intent);
+                });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private void requestPermission(){
+        isWritePermissionGranted = ContextCompat.checkSelfPermission(
+                this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+
+        if (isWritePermissionGranted) {
+            // You can use the API that requires the permission.
+
+        } else {
+            // You can directly ask for the permission.
+            // The registered ActivityResultCallback gets the result of this request.
+            mPermissionResultLauncher.launch(
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+    }
+
 }
